a94f15535524f781dbb7413848f1ed8b62b38a25
Davi Bogner <davibogner@hotmail.com>
2022-10-07 20:48:20 -0300

    modified:   .next/cache/webpack/client-development/0.pack 	modified:   .next/cache/webpack/client-development/1.pack 	modified:   .next/cache/webpack/client-development/index.pack 	modified:   .next/cache/webpack/client-development/index.pack.old 	modified:   .next/cache/webpack/server-development/0.pack 	modified:   .next/cache/webpack/server-development/1.pack 	modified:   .next/cache/webpack/server-development/index.pack 	modified:   .next/cache/webpack/server-development/index.pack.old 	modified:   .next/server/pages/index.js 	modified:   .next/static/chunks/pages/index.js 	modified:   .next/static/chunks/webpack.js 	deleted:    .next/static/webpack/3f3a3100e87e3cfd.webpack.hot-update.json 	deleted:    .next/static/webpack/7dbccfaba13c0e00.webpack.hot-update.json 	deleted:    .next/static/webpack/pages/index.7dbccfaba13c0e00.hot-update.js 	deleted:    .next/static/webpack/webpack.3f3a3100e87e3cfd.hot-update.js 	deleted:    .next/static/webpack/webpack.7dbccfaba13c0e00.hot-update.js 	modified:   .next/static/webpack/webpack.fb036ec250a651b0.hot-update.js 	modified:   .next/trace 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/index.js
        Untracked files:
    	.next/cache/webpack/client-development-fallback/
    	.next/cache/webpack/client-development/2.pack
    	.next/cache/webpack/client-development/3.pack
    	.next/cache/webpack/client-development/4.pack
    	.next/cache/webpack/client-development/5.pack
    	.next/cache/webpack/client-development/6.pack
    	.next/cache/webpack/client-development/7.pack
    	.next/cache/webpack/server-development/2.pack
    	.next/cache/webpack/server-development/3.pack
    	.next/cache/webpack/server-development/4.pack
    	.next/cache/webpack/server-development/5.pack
    	.next/cache/webpack/server-development/6.pack
    	.next/static/webpack/483e5e0824d28aee.webpack.hot-update.json
    	.next/static/webpack/webpack.483e5e0824d28aee.hot-update.js
    	node_modules/.bin/jsesc
    	node_modules/.bin/jsesc.cmd
    	node_modules/.bin/jsesc.ps1
    	node_modules/.bin/parser
    	node_modules/.bin/parser.cmd
    	node_modules/.bin/parser.ps1
    	node_modules/@babel/
    	node_modules/@emotion/
    	node_modules/@jridgewell/
    	node_modules/ansi-styles/
        node_modules/babel-plugin-styled-components/
    	node_modules/babel-plugin-syntax-jsx/
    	node_modules/camelize/
    	node_modules/chalk/
    	node_modules/color-convert/
    	node_modules/color-name/
    	node_modules/css-color-keywords/
    	node_modules/css-to-react-native/
    	node_modules/debug/
    	node_modules/escape-string-regexp/
    	node_modules/globals/
    	node_modules/has-flag/
    	node_modules/hoist-non-react-statics/
    	node_modules/jsesc/
    	node_modules/lodash/
    	node_modules/ms/
    	node_modules/picomatch/
    	node_modules/postcss-value-parser/
    	node_modules/react-is/
    	node_modules/shallowequal/
    	node_modules/styled-components/
    	node_modules/supports-color/
    	node_modules/to-fast-properties/
        pages/styles.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,60 @@
+import { useState } from 'react';
+import { Div } from './styles';
 function Home() {
-    return <div> OLLA 21212 MUNDOOO!! </div>
+    return (
+        <div>
+            <Contador></Contador>
+            <div> OLAAAA</div>
+            <TempControl></TempControl>            
+            
+        </div>        
+    )             
+}
+function colorControl(){
+    
+}
+function TempControl(){
+    const [temp,setTemp] = useState(20);
+    const [colorTemp,setColorTemp] = useState('red');
+    function addTemp(){
+        setTemp(temp+1);
+        if(temp>=20){
+            setColorTemp('red');
+        }
+    }
+    function decTemp(){
+        setTemp(temp-1);
+        if(temp<20){
+            setColorTemp('blue');
+
+        }
+    }
+    return (      
+                    
+            <Div className={colorTemp}><div>
+                <div className='visor'>{temp}</div>
+            <button onClick={addTemp}>+</button>
+            <button onClick={decTemp}>-</button></div></Div>
+        
+        )
+}
+function Contador(){
+    const [number,setContador] = useState(1);
+    function addNumber(){
+        setContador(number+1);        
+
+    }
+    function resetNumber(){
+        setContador(1);
+    } 
+    return (
+        <div>
+            <div>{number}</div>
+            <button onClick={addNumber}>ADD</button>
+            <button onClick={resetNumber}>RESET</button>
+        </div>
+    )
 }
 
-export default Home
+
+export default Home;
